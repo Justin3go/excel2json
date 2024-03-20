af4e865f2a5fb6b32b2e1526a312151edb371726
justin3go <just@justin3go.com>
2024-03-20 09:04:37 +0800

    feat: support export tableData

--- pages/index.vue
+++ pages/index.vue
@@ -9,7 +9,8 @@
 			prepend-icon="mdi-paperclip"
 		>
 		</v-file-input>
-    <div>{{ result }}</div>
+		<div>{{ mergeResult }}</div>
+		<div>{{ tableResult }}</div>
 	</div>
 </template>
 
@@ -17,36 +18,43 @@
 import Excel from "exceljs";
 
 const file = ref();
-const result = ref("No file selected");
+const mergeResult = ref("No file selected");
+const tableResult = ref("No file selected");
 const workbook = new Excel.Workbook();
-
-const convertFormat = (data: any) => {
-    const result = [];
-    for (const key in data) {
-        const model = data[key].model;
-        const row = model.top - 1;
-        const col = model.left - 1;
-        const rowspan = model.bottom - model.top + 1;
-        const colspan = model.right - model.left + 1;
-        result.push({ row, col, rowspan, colspan });
-    }
-    // Sort the result by row and then by col
-    result.sort((a, b) => a.row - b.row || a.col - b.col);
-    return result;
-};
 
 watch(file, async (newVal) => {
 	if (!newVal) {
-    result.value = "No file selected";
-    return;
-  }
-	console.log("file: ", newVal[0]);
+		mergeResult.value = "No file selected";
+		return;
+	}
+
 	await workbook.xlsx.load(newVal[0]);
 	workbook.eachSheet(function (worksheet, sheetId) {
-    // @ts-ignore
-    result.value = JSON.stringify(convertFormat(worksheet._merges));
+		// @ts-ignore
+		mergeResult.value = JSON.stringify(convertMergeFormat(worksheet._merges));
+		// @ts-ignore
+    tableResult.value = JSON.stringify(convertTableFormat(worksheet._rows));
 	});
 });
+
+const convertMergeFormat = (data: any) => {
+	const result = [];
+	for (const key in data) {
+		const model = data[key].model;
+		const row = model.top - 1;
+		const col = model.left - 1;
+		const rowspan = model.bottom - model.top + 1;
+		const colspan = model.right - model.left + 1;
+		result.push({ row, col, rowspan, colspan });
+	}
+	// Sort the result by row and then by col
+	result.sort((a, b) => a.row - b.row || a.col - b.col);
+	return result;
+};
+
+const convertTableFormat = (data: any) => {
+	return data.map((item: any) => item.values);
+};
 </script>
 
 <style></style>
