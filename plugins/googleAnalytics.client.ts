3f5f06322add4c5102933123caf937c64cb9397e
justin3go <just@justin3go.com>
2024-03-20 10:56:40 +0800

    chore: add google analytics

---
+++ plugins/googleAnalytics.client.ts
@@ -0,0 +1,10 @@
+import VueGtag, { trackRouter } from 'vue-gtag-next'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      id: 'G-DZKHQFN6KY'
+    }
+  })
+  trackRouter(useRouter())
+})
